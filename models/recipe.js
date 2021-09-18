d39ea8cb42ce714ef13c85f3fdedacc6d6e5c9c8
Lea von Oldenburg <lea92.von.oldenburg@gmail.com>
2021-09-18 14:50:11 +0200

    create recipe first version

--- models/recipe.js
+++ models/recipe.js
@@ -1,18 +1,30 @@
 const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
-  text: {
+  title: {
     type: String,
     required: true,
     minlength: 3,
-    maxlength: 1000
+    maxlength: 300
   },
 
-  creator: {
-    type: mongoose.Schema.Types.ObjectId,
+  ingredients: {
+    type: String,
     required: true,
-    ref: 'User'
+    minlength: 3
   },
+
+  instructions: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+
+  // creator: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   required: true,
+  //   ref: 'User'
+  // },
 
   picture: {
     type: String
--- routes/create-recipe.js
+++ routes/create-recipe.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Recipe = require('./../models/recipe');
-//const upload = require('./../middleware/file-upload');
+const upload = require('./../middleware/file-upload');
 
 const routeGuardMiddleware = require('./../middleware/route-guard');
 const recipeRouter = express.Router();
@@ -9,8 +9,21 @@ recipeRouter.get('/', routeGuardMiddleware, (req, res, next) => {
   res.render('create-recipe');
 });
 
-recipeRouter.post('/', routeGuardMiddleware, (req, res, next) => {
-  res.render('/create-recipe');
-});
+recipeRouter.post(
+  '/',
+  routeGuardMiddleware,
+  upload.single('picture'),
+  (req, res, next) => {
+    const { title, ingredients, instructions } = req.body;
+    let picture = req.file;
+    Recipe.create({
+      title,
+      ingredients,
+      instructions
+    }).then(() => {
+      res.redirect('/');
+    });
+  }
+);
 
 module.exports = recipeRouter;
