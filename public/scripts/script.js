01552a1fd20f3f9cf140e890f514663e50784c15
Senimtra <senimtra@hotmail.com>
2021-09-23 20:11:09 +0200

    move ingredient script -> scripts + delete model

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,26 @@
 window.addEventListener('load', () => {
   console.log('Ironmaker app started successfully!');
 }, false);
+
+
+let ingredientCount = 1;
+const ingredientsContainerElement = document.getElementById('ingredient-container');
+const addIngredientButtonElement = document.getElementById('add-ingredient');
+const removeIngredientButtonElement = document.getElementById('remove-ingredient');
+addIngredient = () => {
+  ingredientCount++; const newIngredientElement = document.createElement("input");
+  document.getElementById("ingredient-container").appendChild(newIngredientElement);
+  newIngredientElement.setAttribute('class', 'input-ingredients');
+  newIngredientElement.setAttribute('type', 'text');
+  newIngredientElement.setAttribute('name', 'ingredients');
+  newIngredientElement.setAttribute('placeholder', `Ingredient${ingredientCount}`); console.log(ingredientCount);
+}
+removeIngredient = () => {
+  if (ingredientsContainerElement.lastElementChild.getAttribute('class') === 'input-ingredients') {
+    if (ingredientCount > 0) ingredientCount--;
+    ingredientsContainerElement.removeChild(ingredientsContainerElement.lastElementChild);
+  }
+}
+
+addIngredientButtonElement.addEventListener('click', addIngredient);
+removeIngredientButtonElement.addEventListener('click', removeIngredient);
