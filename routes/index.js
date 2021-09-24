dd1a0db1d0f0efe7d7ce4a70599c175213e6f10d
Senimtra <senimtra@hotmail.com>
2021-09-24 17:12:09 +0200

    add get route recipes home pagination

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,52 @@ const express = require('express');
 const Recipe = require('../models/recipe');
 const router = express.Router();
 
-router.get('/confirmation', (req, res, next) => {
+// ### GET confirmation route ###
+router.get('/confirmation', (req, res) => {
   res.render('confirmation');
 });
 
+// ### GET root route ###
 router.get('/', (req, res, next) => {
+  let recipeCount;
   Recipe.find()
-    .populate('creator', 'username picture')
-    .then((recipes) => {
-      res.render('home', { recipes });
+    // get recipes total
+    .then((count) => {
+      recipeCount = count.length;
+    })
+    // get first page recipes
+    .then(() => {
+      return Recipe.find()
+        .limit(12)
+        .populate('creator', 'username picture')
+        .then((recipes) => {
+          res.render('home', { recipes, recipeCount });
+        });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// ### GET page route ###
+router.get('/:page', (req, res, next) => {
+  const page = Number(req.params.page);
+  let recipeCount;
+  Recipe.find()
+    // get recipes total
+    .then((count) => {
+      recipeCount = count.length;
+    })
+    // get recipes according to page
+    .then(() => {
+      const skipCount = (page - 1) * 12;
+      return Recipe.find()
+        .skip(skipCount)
+        .limit(12)
+        .populate('creator', 'username picture')
+        .then((recipes) => {
+          res.render('home', { recipes, recipeCount });
+        });
     })
     .catch((error) => {
       next(error);
