db7453b0bf6b41fd38f9498d178ac50015442260
Senimtra <senimtra@hotmail.com>
2021-09-24 16:43:33 +0200

    refactor recipe create form script

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,26 +1,52 @@
-window.addEventListener('load', () => {
-  console.log('Ironmaker app started successfully!');
-}, false);
-
-
-let ingredientCount = 1;
-const ingredientsContainerElement = document.getElementById('ingredient-container');
-const addIngredientButtonElement = document.getElementById('add-ingredient');
-const removeIngredientButtonElement = document.getElementById('remove-ingredient');
-addIngredient = () => {
-  ingredientCount++; const newIngredientElement = document.createElement("input");
-  document.getElementById("ingredient-container").appendChild(newIngredientElement);
-  newIngredientElement.setAttribute('class', 'input-ingredients');
-  newIngredientElement.setAttribute('type', 'text');
-  newIngredientElement.setAttribute('name', 'ingredients');
-  newIngredientElement.setAttribute('placeholder', `Ingredient${ingredientCount}`); console.log(ingredientCount);
-}
-removeIngredient = () => {
-  if (ingredientsContainerElement.lastElementChild.getAttribute('class') === 'input-ingredients') {
-    if (ingredientCount > 0) ingredientCount--;
-    ingredientsContainerElement.removeChild(ingredientsContainerElement.lastElementChild);
-  }
-}
-
-addIngredientButtonElement.addEventListener('click', addIngredient);
-removeIngredientButtonElement.addEventListener('click', removeIngredient);
+window.addEventListener(
+  'load',
+  () => {
+    // ##########################
+    // ##  RECIPE CREATE FORM  ##
+    // ##########################
+    //
+    // ### Set ingredient input field count ###
+    let ingredientCount = 1;
+    // ### Get DOM element nodes ###
+    const ingredientsContainerElement = document.getElementById(
+      'ingredient-container'
+    );
+    const addIngredientButtonElement =
+      document.getElementById('add-ingredient');
+    const removeIngredientButtonElement =
+      document.getElementById('remove-ingredient');
+    // ### Add ingredient input field function ###
+    const addIngredient = () => {
+      ingredientCount++;
+      const newIngredientElement = document.createElement('input');
+      document
+        .getElementById('ingredient-container')
+        .appendChild(newIngredientElement);
+      newIngredientElement.setAttribute('class', 'input-ingredients');
+      newIngredientElement.setAttribute('type', 'text');
+      newIngredientElement.setAttribute('name', 'ingredients');
+      newIngredientElement.setAttribute(
+        'placeholder',
+        `Ingredient ${ingredientCount}`
+      );
+    };
+    // ### Remove ingredient input field function ###
+    const removeIngredient = () => {
+      if (ingredientsContainerElement.childElementCount > 3) {
+        ingredientCount--;
+        ingredientsContainerElement.removeChild(
+          ingredientsContainerElement.lastElementChild
+        );
+      }
+    };
+    // ### Add button event listeners ###
+    if (addIngredientButtonElement) {
+      addIngredientButtonElement.addEventListener('click', addIngredient);
+    }
+    if (removeIngredientButtonElement) {
+      removeIngredientButtonElement.addEventListener('click', removeIngredient);
+    }
+    console.log('Ironmaker app started successfully!');
+  },
+  false
+);
