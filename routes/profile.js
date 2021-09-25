0759fae09a64b05c60d920eca0d0a9f4672af128
Senimtra <senimtra@hotmail.com>
2021-09-25 13:02:24 +0200

    remove file upload label text + add label

--- routes/profile.js
+++ routes/profile.js
@@ -2,10 +2,7 @@ const express = require('express');
 const User = require('./../models/user');
 const routeGuardMiddleware = require('./../middleware/route-guard');
 const upload = require('./../middleware/file-upload');
-<<<<<<< Updated upstream
 const Recipe = require('./../models/recipe');
-=======
->>>>>>> Stashed changes
 
 const profileRouter = express.Router();
 
