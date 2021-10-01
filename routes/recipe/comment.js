57dbb72716033be10be196a9e68b9191dd952cfc
“nura-fattah” <nura_fattah@outlook.de>
2021-10-01 22:14:46 +0200

    comment delete problem

--- routes/recipe/comment.js
+++ routes/recipe/comment.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const Recipe = require('../../models/recipe');
-const Comment = require('../../models/comment');
+const Recipe = require('./../../models/recipe');
+const Comment = require('./../../models/comment');
 const routeGuardMiddleware = require('../../middleware/route-guard');
 const recipeRouter = express.Router();
 
@@ -25,4 +25,19 @@ recipeRouter.post('/:id/comment', routeGuardMiddleware, (req, res, next) => {
     });
 });
 
+recipeRouter.post(
+  '/:id/comment/delete',
+  routeGuardMiddleware,
+  (req, res, next) => {
+    const { id } = req.params;
+    const { comment } = req.body;
+    Comment.findByIdAndDelete(comment)
+      .then(() => {
+        res.redirect(`recipe/${id}`);
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
 module.exports = recipeRouter;
